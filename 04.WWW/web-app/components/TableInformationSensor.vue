06d13093594e4e94ff7f1efbc6e298ef11af61d7
hoducviet <ducvietsqt@gmail.com>
2021-05-18 00:08:00 +0700

    add update sensor page

--- 04.WWW/web-app/components/TableInformationSensor.vue
+++ 04.WWW/web-app/components/TableInformationSensor.vue
@@ -3,12 +3,12 @@
     <h2 v-if="!disabledTitle" class="m-0 px-4 py-2 text-sm bg-blue-200 text-blue-600">
       {{ title }}
     </h2>
-    <div class="bg-white shadow-md rounded overflow-x-auto">
-      <table class="min-w-max w-full table-auto">
+    <div class="bg-white overflow-x-auto">
+      <table class="min-w-max w-full table-auto border-gray-200 border-l border-r">
         <thead>
-        <tr class="text-gray-600 border-b border-gray-200 uppercase text-sm leading-normal">
-          <template v-for="field in headers">
-            <th class="py-3 px-3 text-left" :key="field.name">
+        <tr class="text-gray-600 border-b border-t border-gray-200 uppercase text-sm leading-normal">
+          <template v-for="(field, index) in headers">
+            <th v-if="index === 0" class="py-3 px-3 text-center" :colspan="headers.length" :key="field.name">
               {{ field.label }}
             </th>
           </template>
@@ -18,7 +18,7 @@
         <template v-for="(row, index) in items">
           <tr :key="index" class="border-b border-gray-200 hover:bg-gray-100 row-item">
             <template v-for="field in headers">
-              <td :key="`${row.id}-${field.name}`" class="px-3 text-left whitespace-nowrap">
+              <td :key="`${row.id}-${field.name}`" class="px-3 text-center whitespace-nowrap border-r">
                 <div class="content-cell">
                   <tempate v-if="field.type === 'chips'">
                     <div class="-mx-1">
@@ -47,7 +47,7 @@
 <script>
 
 export default {
-  name: "TableInformationDevice",
+  name: "TableInformationSensor",
   props: {
     disabledTitle: {
       type: Boolean,
--- 04.WWW/web-app/pages/sensor.vue
+++ 04.WWW/web-app/pages/sensor.vue
@@ -1,13 +1,515 @@
 <template>
   <div>
-    Sensor
+    <h3 class="inline-block text-xl font-semibold text-blue-600 tracking-tight">
+      HA THANH WATER SUPPLY FACTORY, 30.000m3 Capacity
+    </h3>
+    <p class="mt-1 text-lg text-gray-500">MONITORING & SCALLING SENSOR</p>
+    <div class="mt-6"></div>
+    <div class="group-table bg-white">
+      <h2 class="m-0 px-4 py-2 text-sm bg-blue-200 text-blue-600">
+        {{ tb1.title }}
+      </h2>
+      <div class="flex items-start flex-wrap p-2">
+        <template v-for="(tb, index) in tb1.subs">
+          <TableInformationSensor :key="tb.id"
+                                  class="w-80 m-2"
+                                  disabled-title
+                                  v-bind="tb"/>
+
+        </template>
+      </div>
+    </div>
+    <div class="mt-6"></div>
+    <div class="group-table bg-white">
+      <h2 class="m-0 px-4 py-2 text-sm bg-blue-200 text-blue-600">
+        {{ tb2.title }}
+      </h2>
+      <div class="flex items-start flex-wrap p-2">
+        <template v-for="(tb, index) in tb2.subs">
+          <TableInformationSensor :key="tb.id"
+                                  class="w-80 m-2"
+                                  disabled-title
+                                  v-bind="tb"/>
+        </template>
+      </div>
+    </div>
+    <div class="mt-6"></div>
+    <div class="group-table bg-white">
+      <h2 class="m-0 px-4 py-2 text-sm bg-blue-200 text-blue-600">
+        {{ tb3.title }}
+      </h2>
+      <div class="flex items-start flex-wrap p-2">
+        <template v-for="(tb, index) in tb3.subs">
+          <TableInformationSensor :key="tb.id"
+                                  class="w-80 m-2"
+                                  disabled-title
+                                  v-bind="tb"/>
+        </template>
+      </div>
+    </div>
+    <div class="mt-6"></div>
+    <div class="group-table bg-white">
+      <h2 class="m-0 px-4 py-2 text-sm bg-blue-200 text-blue-600">
+        {{ tb4.title }}
+      </h2>
+      <div class="flex items-start flex-wrap p-2">
+        <template v-for="(tb, index) in tb4.subs">
+          <TableInformationSensor :key="tb.id"
+                                  class="w-80 m-2"
+                                  disabled-title
+                                  v-bind="tb"/>
+        </template>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import TableInformationSensor from "../components/TableInformationSensor";
 export default {
-  name: "sensor",
-  layout: "app"
+  components: {TableInformationSensor},
+  layout: "app",
+  data() {
+    return {
+      tb1: {
+        title: 'RAW PUMP STATION',
+        subs: [
+          {
+            id: 1,
+            headers: [
+              {
+                name: 'value',
+                label: 'PRESSURE - PIT01'
+              },
+              {
+                name: 'unit',
+                label: ''
+              },
+            ],
+            items: [
+              {
+                value: '2.85',
+                unit: 'bar'
+              }
+            ]
+          },
+          {
+            id: 2,
+            headers: [
+              {
+                name: 'value',
+                label: 'FLOW RATE - FIT01'
+              },
+              {
+                name: 'unit',
+                label: ''
+              },
+            ],
+            items: [
+              {
+                value: '10.5',
+                unit: 'm3/h'
+              },
+              {
+                value: '10.5',
+                unit: 'h'
+              },
+              {
+                value: '10.5',
+                unit: 'm3'
+              },
+
+            ]
+          },
+          {
+            id: 3,
+            headers: [
+              {
+                name: 'value',
+                label: 'LEVEL - FIT01'
+              },
+              {
+                name: 'unit',
+                label: ''
+              },
+            ],
+            items: [
+              {
+                value: '2.85',
+                unit: 'bar'
+              }
+            ]
+          },
+          {
+            id: 4,
+            headers: [
+              {
+                name: 'value',
+                label: 'pH - pH01'
+              },
+              {
+                name: 'unit',
+                label: ''
+              },
+            ],
+            items: [
+              {
+                value: '2.85',
+                unit: 'bar'
+              }
+            ]
+          },
+          {
+            id: 5,
+            headers: [
+              {
+                name: 'value',
+                label: 'TURBIDITY - NTU01'
+              },
+              {
+                name: 'unit',
+                label: ''
+              },
+            ],
+            items: [
+              {
+                value: '2.85',
+                unit: 'bar'
+              }
+            ]
+          }
+        ],
+      },
+      tb2: {
+        title: 'PROCESS STATION',
+        subs: [
+          {
+            id: 1,
+            headers: [
+              {
+                name: 'value',
+                label: 'PRESSURE - PIT01'
+              },
+              {
+                name: 'unit',
+                label: ''
+              },
+            ],
+            items: [
+              {
+                value: '2.85',
+                unit: 'bar'
+              }
+            ]
+          },
+          {
+            id: 2,
+            headers: [
+              {
+                name: 'value',
+                label: 'FLOW RATE - FIT01'
+              },
+              {
+                name: 'unit',
+                label: ''
+              },
+            ],
+            items: [
+              {
+                value: '10.5',
+                unit: 'm3/h'
+              },
+              {
+                value: '10.5',
+                unit: 'h'
+              },
+              {
+                value: '10.5',
+                unit: 'm3'
+              },
+
+            ]
+          },
+          {
+            id: 3,
+            headers: [
+              {
+                name: 'value',
+                label: 'LEVEL - FIT01'
+              },
+              {
+                name: 'unit',
+                label: ''
+              },
+            ],
+            items: [
+              {
+                value: '2.85',
+                unit: 'bar'
+              }
+            ]
+          },
+          {
+            id: 4,
+            headers: [
+              {
+                name: 'value',
+                label: 'pH - pH01'
+              },
+              {
+                name: 'unit',
+                label: ''
+              },
+            ],
+            items: [
+              {
+                value: '2.85',
+                unit: 'bar'
+              }
+            ]
+          },
+          {
+            id: 5,
+            headers: [
+              {
+                name: 'value',
+                label: 'TURBIDITY - NTU01'
+              },
+              {
+                name: 'unit',
+                label: ''
+              },
+            ],
+            items: [
+              {
+                value: '2.85',
+                unit: 'bar'
+              }
+            ]
+          }
+        ],
+      },
+      tb3: {
+        title: 'CHEMICAL STATION',
+        subs: [
+          {
+            id: 1,
+            headers: [
+              {
+                name: 'value',
+                label: 'PRESSURE - PIT01'
+              },
+              {
+                name: 'unit',
+                label: ''
+              },
+            ],
+            items: [
+              {
+                value: '2.85',
+                unit: 'bar'
+              }
+            ]
+          },
+          {
+            id: 2,
+            headers: [
+              {
+                name: 'value',
+                label: 'FLOW RATE - FIT01'
+              },
+              {
+                name: 'unit',
+                label: ''
+              },
+            ],
+            items: [
+              {
+                value: '10.5',
+                unit: 'm3/h'
+              },
+              {
+                value: '10.5',
+                unit: 'h'
+              },
+              {
+                value: '10.5',
+                unit: 'm3'
+              },
+
+            ]
+          },
+          {
+            id: 3,
+            headers: [
+              {
+                name: 'value',
+                label: 'LEVEL - FIT01'
+              },
+              {
+                name: 'unit',
+                label: ''
+              },
+            ],
+            items: [
+              {
+                value: '2.85',
+                unit: 'bar'
+              }
+            ]
+          },
+          {
+            id: 4,
+            headers: [
+              {
+                name: 'value',
+                label: 'pH - pH01'
+              },
+              {
+                name: 'unit',
+                label: ''
+              },
+            ],
+            items: [
+              {
+                value: '2.85',
+                unit: 'bar'
+              }
+            ]
+          },
+          {
+            id: 5,
+            headers: [
+              {
+                name: 'value',
+                label: 'TURBIDITY - NTU01'
+              },
+              {
+                name: 'unit',
+                label: ''
+              },
+            ],
+            items: [
+              {
+                value: '2.85',
+                unit: 'bar'
+              }
+            ]
+          }
+        ],
+      },
+      tb4: {
+        title: 'SUPPLY STATION',
+        subs: [
+          {
+            id: 1,
+            headers: [
+              {
+                name: 'value',
+                label: 'PRESSURE - PIT01'
+              },
+              {
+                name: 'unit',
+                label: ''
+              },
+            ],
+            items: [
+              {
+                value: '2.85',
+                unit: 'bar'
+              }
+            ]
+          },
+          {
+            id: 2,
+            headers: [
+              {
+                name: 'value',
+                label: 'FLOW RATE - FIT01'
+              },
+              {
+                name: 'unit',
+                label: ''
+              },
+            ],
+            items: [
+              {
+                value: '10.5',
+                unit: 'm3/h'
+              },
+              {
+                value: '10.5',
+                unit: 'h'
+              },
+              {
+                value: '10.5',
+                unit: 'm3'
+              },
+
+            ]
+          },
+          {
+            id: 3,
+            headers: [
+              {
+                name: 'value',
+                label: 'LEVEL - FIT01'
+              },
+              {
+                name: 'unit',
+                label: ''
+              },
+            ],
+            items: [
+              {
+                value: '2.85',
+                unit: 'bar'
+              }
+            ]
+          },
+          {
+            id: 4,
+            headers: [
+              {
+                name: 'value',
+                label: 'pH - pH01'
+              },
+              {
+                name: 'unit',
+                label: ''
+              },
+            ],
+            items: [
+              {
+                value: '2.85',
+                unit: 'bar'
+              }
+            ]
+          },
+          {
+            id: 5,
+            headers: [
+              {
+                name: 'value',
+                label: 'TURBIDITY - NTU01'
+              },
+              {
+                name: 'unit',
+                label: ''
+              },
+            ],
+            items: [
+              {
+                value: '2.85',
+                unit: 'bar'
+              }
+            ]
+          }
+        ],
+      },
+
+    }
+  }
 }
 </script>
 
