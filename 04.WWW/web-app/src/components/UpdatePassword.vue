4d894570ca83dffae2e6464e5b5029e829469f5f
hoducviet <ducvietsqt@gmail.com>
2021-07-24 22:11:53 +0700

    update change account

--- 04.WWW/web-app/src/components/UpdatePassword.vue
+++ 04.WWW/web-app/src/components/UpdatePassword.vue
@@ -1,12 +1,16 @@
 <template>
-  <div>
-    <div>
-      <v-img class="md:w-1/6 m-auto block" src="/img/logo-phuc-thinh-22.png"></v-img>
-    </div>
-    <h3 class="text-center text-xl font-semibold text-blue-600 tracking-tight">
-      Thay đổi mật khẩu
-    </h3>
-    <div class="box-form m-auto">
+  <v-card width="450px"
+          class="mx-auto"
+          elevation="0"
+          :disabled="submitting"
+          :loading="submitting">
+    <v-card-title class="text-h5">
+      {{ $t('layout.labelUpdatePassword') }}
+    </v-card-title>
+    <v-card-text>
+      Enter your email, phone, or username and we'll send you a link to get back into your account.
+    </v-card-text>
+    <v-card-text>
       <validation-observer
           ref="observer"
           v-slot="{ invalid }"
@@ -21,7 +25,6 @@
                 v-model="new_password"
                 :error-messages="errors"
                 label="Mật khẩu mới"
-                prepend-icon="mdi-lock"
                 required
             ></v-text-field>
           </validation-provider>
@@ -36,27 +39,31 @@
                 :error-messages="errors"
                 label="Nhập lại mật khẩu"
                 required
-                prepend-icon="mdi-lock"
             ></v-text-field>
           </validation-provider>
-          <v-btn
-              class="mr-4"
-              type="submit"
-              :disabled="invalid"
-          >
-            Cập nhật
-          </v-btn>
-          <v-btn @click="clear">
-            Xóa
-          </v-btn>
+          <v-row>
+            <v-col>
+              <v-btn @click="clear" width="100%">
+                {{ $t('layout.btnReset') }}
+              </v-btn>
+            </v-col>
+            <v-col>
+              <v-btn width="100%"
+                     class="mr-4"
+                     type="submit"
+                     color="primary"
+                     :disabled="invalid"
+              >
+                {{ $t('layout.btnUpdate') }}
+              </v-btn>
+            </v-col>
+          </v-row>
         </form>
       </validation-observer>
-    </div>
-
-  </div>
+    </v-card-text>
+  </v-card>
 </template>
 <script>
-import {mapGetters} from 'vuex';
 import {required, min} from 'vee-validate/dist/rules';
 import {extend, ValidationObserver, ValidationProvider, setInteractionMode} from 'vee-validate';
 import {changePassword} from "../api/auth";
@@ -80,16 +87,8 @@ export default {
     return {
       new_password: '',
       confirm_password: '',
+      submitting: false,
     }
-  },
-  computed: {
-    ...mapGetters({
-      user: 'auth/user',
-      statusAPI: 'auth/status_api',
-      messageError: 'auth/messageError'
-    }),
-  },
-  mounted() {
   },
   methods: {
     async submit() {
@@ -98,16 +97,17 @@ export default {
         let new_password = this.new_password;
         let confirm_password = this.confirm_password;
         try {
+          this.submitting = true
           await changePassword({new_password, confirm_password});
           const message = 'Cập nhật mật khẩu thành công!';
-          this.showDialog(message)
-        }catch (e) {
-          this.showDialog('Error')
+          this.$notify({message, type: 'success'})
+          this.clear()
+        } catch (e) {
+          this.$notify({message: e.message, type: 'error'})
+        } finally {
+          this.submitting = false
         }
       }
-    },
-    showDialog(message) {
-      this.$notify({message})
     },
     clear() {
       this.new_password = ''
