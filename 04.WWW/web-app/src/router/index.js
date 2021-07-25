0e2c2129b2846ccd53734c93d566cf57bf0545ab
hoducviet <ducvietsqt@gmail.com>
2021-07-25 16:46:52 +0700

    update fix prod config

--- 04.WWW/web-app/src/router/index.js
+++ 04.WWW/web-app/src/router/index.js
@@ -10,22 +10,22 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => import(/* webpackChunkName: "main.layout" */ '../layouts/main.layout'),
+    component: () => import('../layouts/main.layout'),
     beforeEnter: authenticated,
     children: [
       {
         path: '',
         components: {
-          'default': () => import(/* webpackChunkName: "Home" */ '../pages/index'),
-          'local-menu': () => import(/* webpackChunkName: "customers-local-menu" */ '../components/local-menu/LocalMenuCustomer'),
+          'default': () => import('../pages/index'),
+          'local-menu': () => import('../components/local-menu/LocalMenuCustomer'),
         },
       },
       {
         path: '/customers',
         name: 'customers',
         components: {
-          'default': () => import(/* webpackChunkName: "customers" */ '../pages/cutomers'),
-          'local-menu': () => import(/* webpackChunkName: "customers-local-menu" */ '../components/local-menu/LocalMenuCustomer'),
+          'default': () => import('../pages/cutomers'),
+          'local-menu': () => import('../components/local-menu/LocalMenuCustomer'),
         },
       },
 
@@ -33,80 +33,80 @@ const routes = [
         path: '/customers/:customer',
         name: 'customers-customer',
         components: {
-          'default': () => import(/* webpackChunkName: "customers-customer" */ '../pages/_customer'),
-          'local-menu': () => import(/* webpackChunkName: "customers-local-menu" */ '../components/local-menu/LocalMenuCustomer'),
+          'default': () => import('../pages/_customer'),
+          'local-menu': () => import('../components/local-menu/LocalMenuCustomer'),
         },
       },
       {
         path: '/customers/:customer/factory/:factory/account',
-        name: 'customers-customer-account',
+        name: 'account-factory',
         components: {
-          'local-menu': () => import(/* webpackChunkName: "customers-local-menu" */ '../components/local-menu/LocalMenuCustomer'),
-          'default': () => import(/* webpackChunkName: "account" */ '../pages/account'),
-          'local-toolbar': () => import(/* webpackChunkName: "customers-local-toolbar" */ '../components/local-toolbar/toolbar-factory'),
+          'local-menu': () => import('../components/local-menu/LocalMenuCustomer'),
+          'default': () => import('../pages/account'),
+          'local-toolbar': () => import('../components/local-toolbar/toolbar-factory'),
         },
       },
       {
         path: '/customers/:customer/factory/:factory/account-info',
         name: 'customers-customer-account-info',
         components: {
-          'local-menu': () => import(/* webpackChunkName: "customers-local-menu" */ '../components/local-menu/LocalMenuCustomer'),
-          'default': () => import(/* webpackChunkName: "account-info" */ '../pages/account-info'),
-          'local-toolbar': () => import(/* webpackChunkName: "customers-local-toolbar" */ '../components/local-toolbar/toolbar-factory'),
+          'local-menu': () => import( '../components/local-menu/LocalMenuCustomer'),
+          'default': () => import('../pages/account-info'),
+          'local-toolbar': () => import('../components/local-toolbar/toolbar-factory'),
         },
       },
       {
         path: '/customers/:customer/factory/:factory/account-password',
         name: 'customers-customer-account-password',
         components: {
-          'local-menu': () => import(/* webpackChunkName: "customers-local-menu" */ '../components/local-menu/LocalMenuCustomer'),
-          'default': () => import(/* webpackChunkName: "account-password" */ '../pages/account-password'),
-          'local-toolbar': () => import(/* webpackChunkName: "customers-local-toolbar" */ '../components/local-toolbar/toolbar-factory'),
+          'local-menu': () => import('../components/local-menu/LocalMenuCustomer'),
+          'default': () => import('../pages/account-password'),
+          'local-toolbar': () => import('../components/local-toolbar/toolbar-factory'),
         },
       },
       {
         path: '/customers/:customer/factory/:factory/device',
         name: 'customers-customer-account-password',
         components: {
-          'local-menu': () => import(/* webpackChunkName: "customers-local-menu" */ '../components/local-menu/LocalMenuCustomer'),
-          'local-toolbar': () => import(/* webpackChunkName: "customers-local-toolbar" */ '../components/local-toolbar/toolbar-factory'),
-          'default': () => import(/* webpackChunkName: "device" */ '../pages/factory/device'),
+          'local-menu': () => import('../components/local-menu/LocalMenuCustomer'),
+          'local-toolbar': () => import('../components/local-toolbar/toolbar-factory'),
+          'default': () => import('../pages/factory/device'),
         },
       },
       {
         path: '/customers/:customer/factory/:factory/chart',
         name: 'customers-customer-account-password',
         components: {
-          'local-menu': () => import(/* webpackChunkName: "customers-local-menu" */ '../components/local-menu/LocalMenuCustomer'),
-          'local-toolbar': () => import(/* webpackChunkName: "customers-local-toolbar" */ '../components/local-toolbar/toolbar-factory'),
-          'default': () => import(/* webpackChunkName: "chart" */ '../pages/factory/chart'),
+          'local-menu': () => import('../components/local-menu/LocalMenuCustomer'),
+          'local-toolbar': () => import('../components/local-toolbar/toolbar-factory'),
+          'default': () => import('../pages/factory/chart'),
         },
       },
       {
         path: '/customers/:customer/factory/:factory/maps',
         name: 'customers-customer-account-password',
         components: {
-          'local-menu': () => import(/* webpackChunkName: "customers-local-menu" */ '../components/local-menu/LocalMenuCustomer'),
-          'local-toolbar': () => import(/* webpackChunkName: "customers-local-toolbar" */ '../components/local-toolbar/toolbar-factory'),
-          'default': () => import(/* webpackChunkName: "maps" */ '../pages/factory/maps'),
+          'local-menu': () => import('../components/local-menu/LocalMenuCustomer'),
+          'local-toolbar': () => import('../components/local-toolbar/toolbar-factory'),
+          'default': () => import( '../pages/factory/maps'),
         },
       },
       {
         path: '/customers/:customer/factory/:factory/overview',
         name: 'customers-customer-account-password',
         components: {
-          'local-menu': () => import(/* webpackChunkName: "customers-local-menu" */ '../components/local-menu/LocalMenuCustomer'),
-          'local-toolbar': () => import(/* webpackChunkName: "customers-local-toolbar" */ '../components/local-toolbar/toolbar-factory'),
-          'default': () => import(/* webpackChunkName: "overview" */ '../pages/factory/overview'),
+          'local-menu': () => import('../components/local-menu/LocalMenuCustomer'),
+          'local-toolbar': () => import( '../components/local-toolbar/toolbar-factory'),
+          'default': () => import('../pages/factory/overview'),
         },
       },
       {
         path: '/customers/:customer/factory/:factory/sensor',
         name: 'customers-customer-sensor',
         components: {
-          'local-menu': () => import(/* webpackChunkName: "customers-local-menu" */ '../components/local-menu/LocalMenuCustomer'),
-          'local-toolbar': () => import(/* webpackChunkName: "customers-local-toolbar" */ '../components/local-toolbar/toolbar-factory'),
-          'default': () => import(/* webpackChunkName: "sensor" */ '../pages/factory/sensor'),
+          'local-menu': () => import('../components/local-menu/LocalMenuCustomer'),
+          'local-toolbar': () => import('../components/local-toolbar/toolbar-factory'),
+          'default': () => import('../pages/factory/sensor'),
         },
       },
 
@@ -118,18 +118,18 @@ const routes = [
     name: 'auth',
     meta: {},
     beforeEnter: authenticate,
-    component: () => import(/* webpackChunkName: "auth-layout" */ '../layouts/auth.layout'),
+    component: () => import('../layouts/auth.layout'),
     children: [
       {
         path: '/auth/login',
         name: 'auth-login',
-        component: () => import(/* webpackChunkName: "auth-login" */ '../pages/auth/login'),
+        component: () => import('../pages/auth/login'),
       }
     ]
   },
   {
     path: '*',
-    component: () => import(/* webpackChunkName: "about" */ '../pages/404.vue')
+    component: () => import('../pages/404.vue')
   },
 ]
 
--- 04.WWW/web-app/vue.config.js
+++ 04.WWW/web-app/vue.config.js
@@ -1,6 +1,22 @@
 const dotenv = require('dotenv')
 dotenv.config()
 module.exports = {
+  publicPath: '/',
+  filenameHashing: true,
+  runtimeCompiler: true,
+  configureWebpack: {
+    devtool: 'source-map',
+    output: {
+      filename: () => 'js/[name].[hash:16].js',
+      chunkFilename: 'js/[name].[hash:16].js',
+    },
+    resolve: {
+      extensions: ['.js', '.vue', '.json'],
+    },
+    plugins: [
+      // new VuetifyLoaderPlugin(),
+    ],
+  },
   transpileDependencies: [
     'vuetify'
   ]
