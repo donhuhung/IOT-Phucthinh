4bb0d113ff46f62dac80d3eacdb63bb52db6bae6
Như Hưng <hungdn0502@gmail.com>
2021-08-10 15:51:40 +0700

    update sync overview realtime

--- 04.WWW/web-app/src/pages/factory/overview.vue
+++ 04.WWW/web-app/src/pages/factory/overview.vue
@@ -1,13 +1,47 @@
 <template>
   <div>
-    <v-img :src="factory.overview"/>
+    <template v-if="loading">
+      <v-skeleton-loader type="table" width="100%"/>
+    </template>
+    <v-img v-if="showImage" :src="factory.overview" @load="onImageLoad"/>
   </div>
 </template>
 
 <script>
-import factoryMixin from "@/mixins/factoryMixin"
+import {getDetailFactory} from "@/api/app";
+
 export default {
-  mixins: [factoryMixin],
+  data() {
+    return {
+      factory: {},
+      showImage: true,
+      loading: true,
+    }
+  },
+  async mounted() {
+    await this.detailFactory()
+  },
+  methods: {
+    async detailFactory(){
+      let factory_id = this.$route.params.factory;
+      const res = await getDetailFactory(factory_id)
+      this.factory = res.data.data
+      this.syncDetailFactory()
+    },
+    syncDetailFactory() {
+      this.timer = setInterval(async () => {
+        this.loading = true;
+        this.showImage = false
+        let factory_id = this.$route.params.factory;
+        const res = await getDetailFactory(factory_id)
+        this.factory = res.data.data
+        this.showImage = true
+      }, 45000);
+    },
+    onImageLoad(){
+      this.loading = false;
+    }
+  }
 }
 </script>
 
--- 04.WWW/web-app/vue.config.js
+++ 04.WWW/web-app/vue.config.js
@@ -1,9 +1,7 @@
 const dotenv = require('dotenv')
 dotenv.config()
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/'
-    : '/',
+  publicPath: '/',
   filenameHashing: true,
   runtimeCompiler: true,
   configureWebpack: {
